76dc0a1c8bd53f1296dd15d2e80464410463078c
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-23 15:22:43 -0700

    twig template and silex route for stylists will now list stylists from database

--- app/app.php
+++ app/app.php
@@ -4,12 +4,21 @@
     require_once __DIR__."/../src/Stylist.php";
     $app = new Silex\Application();
 
+    $server = 'mysql:host=localhost;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
       'twig.path' => __DIR__.'/../views'
       ));
 
     $app->get("/", function () use ($app) {
       return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/stylists", function () use ($app) {
+      return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
 
     return $app;
