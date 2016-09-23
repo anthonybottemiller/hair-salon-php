0b2bca52f68308ced6e81655865600746d2e83bf
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-23 16:08:51 -0700

    getname for customer passes test

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+    class Customer
+    {
+      private $name;
+
+      function __construct($name)
+      {
+        $this->name = $name;
+      }
+
+      function getName()
+      {
+        return $this->name;
+      }
+    }
+?>
