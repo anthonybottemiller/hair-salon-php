ff12e4dd8e51d2406100b69f0b1de61c9431b6f9
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-23 10:17:10 -0700

    adds first test to get id of stylist

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,9 +1,26 @@
 <?php
 
-  require_once "src/Stylist.php";
+    require_once "src/Stylist.php";
 
-  class StylistTest extends PHPUnit_framework_TestCase
-  {
 
-  }
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+        function testGetStylistId()
+        {
+          $name = "Trisha Rose";
+          $test_stylist = new Stylist($name);
+
+          $result = $test_stylist->getId;
+
+          $this->assertEquals(true, is_numeric($result));
+        }
+
+    }
+
   ?>
